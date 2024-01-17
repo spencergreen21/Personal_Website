1e1c7b868d3bf96f7d902f15b72f868333416839
green <greenspencer.10@gmail.com>
2024-01-17 14:07:55 -0500

    Initial commit

---
+++ README.txt
@@ -0,0 +1,19 @@
+Dimension by HTML5 UP
+html5up.net | @ajlkn
+
+
+Website link can be found here: _________
+
+
+
+Credits:
+
+	Demo Images:
+		Unsplash (unsplash.com)
+
+	Icons:
+		Font Awesome (fontawesome.io)
+
+	Other:
+		jQuery (jquery.com)
+		Responsive Tools (github.com/ajlkn/responsive-tools)
---
+++ main.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, flash
+from flask_mail import Mail, Message
+import os
+
+app = Flask(__name__, static_url_path='/static')
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
+
+
+# Configure Flask-Mail
+app.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER')
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL')
+app.config['MAIL_PASSWORD'] = os.environ.get('PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('EMAIL')
+
+mail = Mail(app)
+
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+
+        if not name or not email or not message:
+            flash('Please fill out all fields.', 'error')
+        else:
+            msg = Message('New Contact Form Submission', recipients=[os.environ.get('EMAIL')])
+            msg.body = f"Name: {name}\nEmail: {email}\n\nMessage:\n{message}"
+            mail.send(msg)
+
+            flash('Your message has been sent!', 'success')
+
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False)
